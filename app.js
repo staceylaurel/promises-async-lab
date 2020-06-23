c2ca34b214b6df44fb0e5a73bf263ec580fa05ef
staceylaurel <64053039+staceylaurel@users.noreply.github.com>
2020-06-23 11:10:18 -0500

    Add files via upload
    final lab with notes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Promise.then() Chaining
+
 // slowMath.add(6, 2).then(num => {
 //     console.log(num);
 //     return slowMath.multiply(num, 2);
@@ -29,38 +31,28 @@
 // });
 
 ////Async/Await
-let doMath = (slowMath)
-
+const await = async () => {
 try {
-    let message = slowMath.add(6, 2) 
-    if (message === 4) {(num => {
-    console.log(num);
-    } else {let err = new Error('calculating error');
-    reject(err);
-}return slowMath.multiply(num, 2);
-// })try(num => {
-//     console.log(num);
-//     return slowMath.divide(num,4);
-// })try(num => {
-//     console.log(num);
-//     return slowMath.subtract(num, 3);
-// })try(num => {
-//     console.log(num)
-//     return slowMath.add(num, 98);
-// })try(num => {
-//     console.log(num)
-// return slowMath.divide(num,2);
-// })try(num => {
-//     console.log(num)
-//     return slowMath.multiply(num, 50);
-// })try(num => {
-//     console.log(num)
-//     return slowMath.divide(num,40);
-// })try(num =>{
-//     console.log(num)
-//     return slowMath.add(num, 32);
-// })try(num => {
-//     console.log(`The final result is ${num}`)
-// }).catch(err => {
-//     console.log(err)
-// });
+    let result = await slowMath.add(1,1);
+    console.log(result);
+    result = await slowMath.multiply(result, 2)
+    console.log(result);
+    result = await slowMath.remainder(result, 4);
+    console.log(result);
+    result = await slowMath.subtract(result, 3);
+    console.log(result);
+    result = await slowMath.add(result, 98);
+    console.log(result);
+    result = await slowMath.remainder(result, 2);
+    console.log(result);
+    result = await slowMath.multiply(result, 50);
+    console.log(result);
+    result = await slowMath.remainder(result, 40);
+    console.log(result);
+    result = await slowMath.add(result, 32);
+    console.log(`The final result is ${result}`);
+} catch (e) {
+    console.log(e);
+}
+}
+await();
